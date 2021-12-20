ee325e503dda800ee565f70fbcb00ce979dfe234
Duy Nguyen <you@example.com>
2021-12-20 17:18:22 +0700

    up

---
+++ ss3_views_thymeleaf/exercise/product-manager-thymleaf/src/main/java/product_manager/controller/ProductController.java
@@ -0,0 +1,76 @@
+package product_manager.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import product_manager.entity.Product;
+import product_manager.service.IProductService;
+
+@Controller
+public class ProductController {
+
+    @Autowired
+    IProductService productService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("products", productService.findAll());
+        return "index";
+    }
+    @GetMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("product", new Product());
+        return "create";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute("product") Product product, RedirectAttributes redirect) {
+        product.setId((int)(Math.random() * 100));
+        productService.save(product);
+        redirect.addFlashAttribute("message", "Create product Done!");
+        return "redirect:/";
+    }
+    @GetMapping("{id}/edit")
+    public String edit(@PathVariable int id, Model model) {
+        model.addAttribute("product", productService.findById(id));
+        return "edit";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute("product") Product product, RedirectAttributes redirect) {
+        productService.update(product.getId(), product);
+        redirect.addFlashAttribute("message", "Update Done");
+        return "redirect:/";
+    }
+    @GetMapping("{id}/delete")
+    public String delete(@PathVariable int id, Model model,RedirectAttributes redirect) {
+        productService.remove(id);
+        redirect.addFlashAttribute("message", "Delete Done");
+        return "redirect:/";
+    }
+
+
+//    @PostMapping("/delete")
+//    public String delete(@RequestParam("idProduct") int id, RedirectAttributes redirect) {
+//        productService.remove(id);
+//        redirect.addFlashAttribute("message", "Delete Done");
+//        return "redirect:/";
+//    }
+
+    @GetMapping("{id}/view")
+    public String view(@PathVariable int id, Model model) {
+        model.addAttribute("product", productService.findById(id));
+        return "view";
+    }
+    @PostMapping("/search")
+    public String search(@RequestParam("nameProduct") String name, Model model,RedirectAttributes redirect) {
+        model.addAttribute("product",productService.search(name));
+        if (productService.search(name)==null){
+            redirect.addFlashAttribute("message", "not found");
+            return "redirect:/";
+        }
+        return "view";
+    }
+}
---
+++ ss3_views_thymeleaf/exercise/product-manager-thymleaf/src/main/java/product_manager/service/IProductService.java
@@ -0,0 +1,19 @@
+package product_manager.service;
+
+import product_manager.entity.Product;
+
+import java.util.List;
+
+public interface IProductService {
+    List<Product> findAll();
+
+    void save(Product product);
+
+    Product findById(int id);
+
+    void update(int id, Product product);
+
+    void remove(int id);
+
+    Product search(String name);
+}
